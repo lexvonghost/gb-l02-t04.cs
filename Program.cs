20b647bf5e0d12a22be820da7689675eb9bc2575
lexvonghost <lex_von_tost@mail.ru>
2022-07-24 21:42:11 +0300

    feat: решил задачу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n >= 1 && n <= 5)
+{
+    Console.WriteLine("Нет, день рабочий.");
+}
+else if (n >= 6 && n <= 7)
+{
+    Console.WriteLine("Да, день выходной.");
+}
+else
+{
+    Console.WriteLine("Неверный ввод");
+}
